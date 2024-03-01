63f0bc5d63b67fc3c81a2162552a019d9d315cee
YashRuikar <yashruikar04@gmail.com>
2024-03-01 19:33:17 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { FlexContent, Hero, Sales, Stories } from "./components";
-import { heroapi, popularsales, toprateslaes, highlight , sneaker, story} from "./data/data.js";
+import { FlexContent, Footer, Hero, Sales, Stories } from "./components";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+  story,
+  footerAPI,
+} from "./data/data.js";
 
 const App = () => {
   return (
-    <main className="flex flex-col gap-16 relative">
-      <Hero heroapi={heroapi} />
-      <Sales endpoint={popularsales} ifExists />
-      <FlexContent endpoint={highlight} ifExists />
-      <Sales endpoint={toprateslaes} />
-      <FlexContent endpoint={sneaker} />
-      <Stories story={story} />
-    </main>
+    <div>
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
+    </div>
   );
 };
 
